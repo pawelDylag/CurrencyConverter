0d0aa790c3c75d63f99b90d2e02f1eb2cca96804
paweldylag <pawel.dylag@estimote.com>
2019-03-29 22:28:47 +0100

    Create repository that uses network and local storage

---
+++ app/src/main/java/com/paweldylag/currencyconverter/repository/NetworkAndLocalStorageCurrencyRepository.kt
@@ -0,0 +1,68 @@
+package com.paweldylag.currencyconverter.repository
+
+import com.paweldylag.currencyconverter.repository.models.CurrencyModel
+import com.paweldylag.currencyconverter.repository.models.ExchangeRatesModel
+import com.paweldylag.currencyconverter.repository.models.ModifiedCurrenciesModel
+import com.wafel.skald.api.createLogger
+import io.reactivex.Flowable
+import io.reactivex.Single
+import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+/**
+ * Created by Pawel Dylag
+ */
+
+class NetworkAndLocalStorageCurrencyRepository @Inject constructor(
+    private val networkService: CurrencyNetworkService,
+    private val localStorageService: CurrencyLocalStorageService
+) : CurrencyRepository {
+
+    private val logger = createLogger(this::class.java)
+
+    override fun getCurrencyExchangeRates(base: CurrencyModel): Single<ExchangeRatesModel> =
+        networkService.getCurrencyExchangeRates(base)
+            .doOnSuccess {
+                localStorageService.updateCurrencyExchangeRates(it)
+            }
+            .onErrorResumeNext {
+                logger.debug("Unable to get exchange rates from network: ${it.message}")
+                localStorageService.getCurrencyExchangeRates(base)
+            }
+            .doOnError {
+                logger.debug("Unable to get exchange rates from local storage: ${it.message}")
+            }
+            .map { ExchangeRatesModel(base, it) }
+
+    override fun observeCurrenciesListModifications(): Flowable<ModifiedCurrenciesModel> =
+        localStorageService.observeCurrencyListModifications().map {
+            ModifiedCurrenciesModel(it)
+        }.doOnNext {
+            logger.debug("Got new modifications list: ${it.currenciesByTimestamp.toList().joinToString {
+                "${it.first.currency.currencyCode} : ${it.second}" }}")
+        }
+
+    override fun observeCurrencyExchangeRates(
+        base: CurrencyModel,
+        period: Long,
+        unit: TimeUnit
+    ): Flowable<ExchangeRatesModel> =
+        Flowable.interval(period, unit).flatMap {
+            getCurrencyExchangeRates(base).toFlowable()
+        }
+
+
+    override fun observeUserDefinedCurrencyAmount(): Flowable<BigDecimal> =
+        localStorageService.observeUserDefinedCurrencyAmount().doOnNext {
+            logger.debug("Got new user currency amount: $it")
+        }
+
+    override fun setUserDefinedCurrency(currency: CurrencyModel) =
+        localStorageService.setUserDefinedCurrency(currency)
+
+
+    override fun setUserDefinedCurrencyAmount(amount: BigDecimal) =
+        localStorageService.setUserDefinedCurrencyAmount(amount)
+
+}
