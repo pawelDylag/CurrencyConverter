52f5e85b3fe294fce26a20499ba21f9552624d6c
paweldylag <pawel.dylag@estimote.com>
2019-03-29 21:09:36 +0100

    Use kotlin annotation processor in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
 }
 
 android {
